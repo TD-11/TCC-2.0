73680e06431abde9adc8bc7ac54215d11f13e778
TD-11 <tulio.s@escolar.ifrn.edu.br>
2024-11-08 08:26:48 -0300

    coletáveis + pontuação incompleta

--- Jogão 2/Assets/Scripts/GameController.cs
+++ Jogão 2/Assets/Scripts/GameController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public static GameController instance;
+    
+    public int totalScore;
+    public TMP_Text text;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +19,8 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        text.text = totalScore.ToString();
     }
+    
+    
 }
